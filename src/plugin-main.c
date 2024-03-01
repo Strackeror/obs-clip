e2dd7691df28befe20b08be471350aa7bddfcd44
Strackeror <rremilavergne@gmail.com>
2024-03-02 00:39:23 +0100

    Add restart buffer on sleep

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -16,20 +16,66 @@ You should have received a copy of the GNU General Public License along
 with this program. If not, see <https://www.gnu.org/licenses/>
 */
 
+#include <obs-frontend-api.h>
 #include <obs-module.h>
 #include <plugin-support.h>
+#include <windows.h>
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
+struct {
+	uint32_t hotkey_id;
+} data;
+
+
+void restart_replay_buffer() {
+	obs_frontend_replay_buffer_stop();
+	Sleep(5000);
+	obs_frontend_replay_buffer_start();
+}
+
+LRESULT CALLBACK window_proc(HWND hwnd, UINT umsg, WPARAM w, LPARAM l)
+{
+	if (umsg == WM_POWERBROADCAST && w == PBT_APMRESUMEAUTOMATIC) {
+		obs_log(LOG_INFO, "Wakeup from sleep detected");
+		if (obs_frontend_replay_buffer_active()) {
+			CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&restart_replay_buffer, 0, 0, NULL);
+		}
+	}
+	return DefWindowProc(hwnd, umsg, w, l);
+}
+
+void load_thread()
+{
+	const WCHAR* name = TEXT("OBS SLEEP");
+	WNDCLASS wc = {
+		.lpfnWndProc = window_proc,
+		.lpszClassName = name,
+	};
+	RegisterClass(&wc);
+
+	HWND hWin = CreateWindow(name, TEXT(""), 0, 0, 0, 0, 0, NULL, NULL, NULL, 0);
+	
+	MSG msg = {};
+	while (true) {
+		BOOL ret = GetMessage(&msg, hWin, 0, 0);
+		if (ret == 0) {
+			break;
+		}
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+}
+
 bool obs_module_load(void)
 {
-	obs_log(LOG_INFO, "plugin loaded successfully (version %s)",
-		PLUGIN_VERSION);
+	obs_log(LOG_INFO, "OBS restart replay on wakeup loaded");
+	CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&load_thread, 0, 0, NULL);
 	return true;
 }
 
 void obs_module_unload(void)
 {
-	obs_log(LOG_INFO, "plugin unloaded");
+	obs_log(LOG_INFO, "OBS restart replay on wakeup unloaded");
 }
